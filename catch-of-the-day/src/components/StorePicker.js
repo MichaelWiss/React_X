0103ecbee7a99b1e955ba37311d39de3a9178f68
MichaelWiss <michael.wiss@gmail.com>
2018-03-08 16:50:29 -0600

    input

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 class StorePicker extends React.Component {
 	render() {
+		
 		return (
 		<form className="store-selector">
 		  <h2>Please enter a store</h2>
+		  <input type="text" required placeholder=Store Name/>
+		  <button type="submit">Visit Store →</button>
 		</form>
 	  )	
 	} 
