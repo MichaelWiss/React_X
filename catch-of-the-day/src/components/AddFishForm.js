f761e096d83b1a01a8432d200816fa2338ea615e
MichaelWiss <michael.wiss@gmail.com>
2018-03-14 12:21:46 -0500

    form inpusts

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -4,7 +4,11 @@ class AddFishForm extends React.Component {
 	render() {
        return (
        <form>
-         <p>It works</p>
+         <input name="name" placeholder="Name" />
+         <input name="price" placeholder="Price" />
+         <input name="status" placeholder="Status" /> 
+         <input name="desc" placeholder="Desc" />
+         <input name="image" placeholder="Image" />
         </form> 
        	);
 	}
