cdb581fe34079b635fb27c18e503495bdfb28ad5
MichaelWiss <michael.wiss@gmail.com>
2018-03-14 11:59:36 -0500

    AddFishForm

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-class addFishForm extends React.Component {
+class AddFishForm extends React.Component {
 	render() {
        return (
-       <div className="AddFishForm">
-       <h2>Inventory</h2>
        <form>
-       
-       </form>
-       </div>
+         <p>It works</p>
+        </form> 
        	);
 	}
 }
