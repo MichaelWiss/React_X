72a649ff8503a048c21392d870c4410e1a6a6a34
MichaelWiss <michael.wiss@gmail.com>
2018-03-21 15:45:42 -0500

    helpers

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
 	render() {
@@ -6,7 +7,11 @@ class Fish extends React.Component {
        return (
        	<li className="menu-fish">
        	 <img src={image} alt={name}/>
-       	 <h3 className="fish-name">{name}</h3>
+       	 <h3 className="fish-name">
+       	    {name}
+       	    <span className="price">{formatPrice(price)}</span>
+
+       	</h3>
        	</li>
        	)
 	}
