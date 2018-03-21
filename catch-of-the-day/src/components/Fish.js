5e51a0dcb24469bbbd720ab9ed6bc0005a56db1f
MichaelWiss <michael.wiss@gmail.com>
2018-03-21 14:30:21 -0500

    fish

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Fish extends React.Component {
+	render() {
+       return (
+       	<div className="single-fish">🐠🐟🐡</div>
+       	)
+	}
+}
+
+
+
+
+
+export default Fish;
