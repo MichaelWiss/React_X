9362a1d1feb2976d99974db7636d703cf6607e75
MichaelWiss <michael.wiss@gmail.com>
2018-04-01 18:05:36 -0500

    event handler for edit form

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -1,17 +1,38 @@
 import React from "react";
 
 class EditFishForm extends React.Component {
+	handleChange = (event) => {
+		console.log(event);
+	}
 	render() {
 		return (
          <div className="fish-edit">
-           <input type="text" name="name" value={this.props.fish.name} />
-           <input type="text" name="price" value={this.props.fish.price} />
-           <select type="text" name="status" value={this.props.fish.status}>
+           <input 
+             type="text" 
+             name="name" 
+             onChange={this.handleChange}
+             value={this.props.fish.name} 
+            />
+           <input 
+             type="text" 
+             name="price"
+             onChange={this.handleChange} 
+             value={this.props.fish.price} 
+            />
+           <select 
+             type="text" 
+             name="status"
+             onChange={this.handleChange} 
+             value={this.props.fish.status}>
              <option value="available">Fresh!</option>
              <option value="unavailable">Sold Out!</option>
            </select>  
            <textarea name="desc" value={this.props.fish.desc} />
-           <input type="text" name="image" value={this.props.fish.image} />
+           <input 
+             type="text" 
+             name="image"
+             onChange={this.handleChange} 
+             value={this.props.fish.image} />
         </div>
        );
 	}
