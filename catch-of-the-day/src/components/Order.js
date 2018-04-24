79476636616c2db513a59dd7d8d46a954172a611
MichaelWiss <michael.wiss@gmail.com>
2018-04-24 15:32:57 -0500

    transitions

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -14,7 +14,7 @@ class Order extends React.Component {
              <CSSTransition 
                classNames="order"
                key={key}
-               timeout={{ enter: 250, exit: 250 }}
+               timeout={{ enter: 5000, exit: 5000 }}
               > 
             <li key={key}>
             Sorry {fish ? fish.name : 'fish'} is no longer available
@@ -26,7 +26,7 @@ return (
     <CSSTransition
       classNames="order"
       key={key}
-      timeout={{ enter: 250, exit: 250 }}
+      timeout={{ enter: 5000, exit: 5000 }}
     >
      <li key={key}>
 		  {count} lbs {fish.name}
