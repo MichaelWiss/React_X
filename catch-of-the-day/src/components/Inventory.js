aff115ec9b0d1e7c6df435da8e22f70397f4e653
MichaelWiss <michael.wiss@gmail.com>
2018-05-03 16:35:33 -0500

    Login

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -2,6 +2,7 @@ import React from "react";
 import AddFishForm from "./AddFishForm";
 import PropTypes from "prop-types";
 import EditFishForm from "./EditFishForm";
+import Login from "./Login";
 
 
 class Inventory extends React.Component {
@@ -12,6 +13,7 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func
   }
 	render() {
+       return <Login />;
        return (
        <div className="inventory">
         <h2>Inventory</h2>
